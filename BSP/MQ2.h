6000d90b06f7259368049092375e689c7b0ce8de
DylanChannn <xiangyuzhan@foxmail.com>
2025-06-25 17:55:42 +0800

    Update MQ2.h

--- BSP/MQ2.h
+++ BSP/MQ2.h
@@ -1,15 +1,15 @@
 /****************************************************************************************************
  * @file        MQ2.h
- * @author      DylanChan(xiangyuzhan@foxmail.com)
+ * @author      DylanChan
  * @version     V1.0
  * @date        2025-06-22
- * @brief       MQ-2���崫��������ͷ�ļ����ṩ�ӿڶ�����Ӳ����������
- * @details     ��ͷ�ļ�����MQ-2���崫���������Ӳ���ӿں͹��ܺ���ԭ�ͣ�
- *              - ADCģ����GPIO�ӿں궨��
- *              - ������ԭʼ���ݶ�ȡ�ӿ�
- *              - ����Ũ��ת���ӿ�
- *              - ����״̬���ӿ�
- * @note        ͨ���궨��MQ2_PPM_ALARM_THRESHOLD�����ñ�����ֵ��Ĭ��10.00PPM��
+ * @brief       MQ-2气体传感器驱动头文件：提供接口定义与硬件参数配置
+ * @details     本头文件定义MQ-2气体传感器所需的硬件接口和功能函数原型：
+ *              - ADC模块与GPIO接口宏定义
+ *              - 传感器原始数据读取接口
+ *              - 气体浓度转换接口
+ *              - 报警状态检测接口
+ * @note        通过宏定义MQ2_PPM_ALARM_THRESHOLD可配置报警阈值（默认10.00PPM）
  ****************************************************************************************************
  */
 #ifndef __MQ2_H
@@ -17,13 +17,13 @@
 #include "stm32f10x.h"
 #include "Delay.h"
 #include "math.h"
-/*************** ADC���ֺ궨�� ****************/
+/*************** ADC部分宏定义 ****************/
 #define    	ADCx							ADC1
 #define    	ADC_CLK							RCC_APB2Periph_ADC1
 /************************************************/
 
-/*************** MQ-2 GPIO�궨�� ****************/
-#define 	MQ2_READ_TIMES					10  //MQ-2������ADCѭ����ȡ����
+/*************** MQ-2 GPIO宏定义 ****************/
+#define 	MQ2_READ_TIMES					10  //MQ-2传感器ADC循环读取次数
 /* PB0/ADC12_IN8 */
 #define		MQ2_AO_GPIO_CLK					RCC_APB2Periph_GPIOB
 #define 	MQ2_AO_GPIO_PORT				GPIOB
